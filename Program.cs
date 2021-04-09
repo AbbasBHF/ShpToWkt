20a7ee54f16fb09ebe50fef865cc1d41e016bc3c
Abbas Baharforoosh <abahar1996@gmail.com>
2021-04-09 18:42:47 +0430

    Initial Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShpToWkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
